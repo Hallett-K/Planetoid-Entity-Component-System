4e61f6b6d165d43332223f04e400d6c05c40a323
Hallett-K <krystalscarlett@outlook.com>
2026-02-13 22:23:50 +0000

    Initial Commit

---
+++ PECS.hpp
@@ -0,0 +1,255 @@
+#ifndef _PECS_HPP_
+#define _PECS_HPP_
+
+#include <typeindex>
+#include <unordered_map>
+#include <vector>
+
+namespace PECS
+{
+
+#ifndef PECS_CUSTOM_TYPE
+    #define PECS_CUSTOM_TYPE size_t
+#endif
+    using EntityID = PECS_CUSTOM_TYPE;
+
+#ifndef PECS_ASSERT
+    #include <cassert>
+    #define PECS_ASSERT assert
+#endif
+
+    template <typename T>
+    class SparseSet
+    {
+    public:
+        SparseSet(size_t MaxEntities)
+            : m_MaxEntities(MaxEntities) 
+        {
+            m_SparseSet.resize(MaxEntities, MaxEntities - 1);
+        }
+
+        ~SparseSet() {}
+
+        T& Get(EntityID entity)
+        {
+            PECS_ASSERT(entity < m_MaxEntities && "Entity ID out of range");
+            PECS_ASSERT(Has(entity) && "Component not found on entity");
+
+            return m_DenseSet[m_SparseSet[entity]].data;
+        }
+
+        template <typename... Args>
+        T& Add(EntityID entity, Args&&... args)
+        {
+            PECS_ASSERT(entity < m_MaxEntities && "Entity ID out of range");
+
+            if (Has(entity))
+            {
+                return m_DenseSet[m_SparseSet[entity]].data;
+            }
+
+            m_DenseSet.push_back({ entity, T(std::forward<Args>(args)...) });
+            m_SparseSet[entity] = m_DenseSet.size() - 1;
+
+            return m_DenseSet.back().data;
+        }
+
+        bool Has(EntityID entity) const
+        {
+            PECS_ASSERT(entity < m_MaxEntities && "Entity ID out of range");
+
+            return m_SparseSet[entity] != m_MaxEntities - 1;
+        }
+
+        bool Remove(EntityID entity)
+        {
+            PECS_ASSERT(entity < m_MaxEntities && "Entity ID out of range");
+
+            if (!Has(entity))
+            {
+                return false;
+            }
+
+            size_t index = m_SparseSet[entity];
+            size_t last = m_DenseSet.size() - 1;
+            DenseSetEntry& lastEntry = m_DenseSet[last];
+            m_DenseSet[index] = std::move(lastEntry);
+
+            m_SparseSet[lastEntry.entity] = index;
+            m_DenseSet.pop_back();
+            m_SparseSet[entity] = m_MaxEntities - 1;
+
+            return true;
+        }
+
+        auto begin() { return m_DenseSet.begin(); }
+        auto end()   { return m_DenseSet.end(); }
+
+        auto begin() const { return m_DenseSet.begin(); }
+        auto end()   const { return m_DenseSet.end(); }
+
+        
+    private:
+        size_t m_MaxEntities;
+        std::vector<EntityID> m_SparseSet;
+
+        struct DenseSetEntry
+        {
+            EntityID entity;
+            T data;
+        };
+
+        std::vector<DenseSetEntry> m_DenseSet;
+    };
+
+    struct ISparseSetWrapper
+    {
+        virtual ~ISparseSetWrapper() = default;
+
+        virtual bool HasEntity(EntityID entity) const = 0;
+        virtual bool RemoveEntity(EntityID entity) = 0;
+    };
+
+    template <typename T>
+    struct SparseSetWrapper : public ISparseSetWrapper
+    {
+        SparseSet<T> set;
+
+        SparseSetWrapper(size_t MaxEntities)
+            : set(MaxEntities) {}
+
+        virtual bool HasEntity(EntityID entity) const override { return set.Has(entity); }
+        virtual bool RemoveEntity(EntityID entity) override { return set.Remove(entity); }
+    };
+
+    class ECSInstance
+    {
+    public:
+        ECSInstance(unsigned int MaxEntities)
+            : m_MaxEntities(MaxEntities) {}
+        ~ECSInstance() 
+        {
+            for (auto& [type, pool] : m_ComponentPools)
+            {
+                delete pool;
+            }
+            m_ComponentPools.clear();
+        }
+
+        EntityID CreateEntity()
+        {
+            if (m_FreeEntities.empty())
+            {
+                PECS_ASSERT(m_NextEntity + 1 < m_MaxEntities && "Max entities reached");
+                return m_NextEntity++;
+            }
+            else 
+            {
+                EntityID id = m_FreeEntities.back();
+                m_FreeEntities.pop_back();
+                return id;
+            }
+        }
+
+        void DeleteEntity(EntityID entity)
+        {
+            PECS_ASSERT(entity < m_MaxEntities && "DeleteEntity called with invalid entity");
+
+            m_FreeEntities.push_back(entity);
+            for (auto&[type, pool] : m_ComponentPools)
+            {
+                pool->RemoveEntity(entity);
+            }
+        }
+
+        template <typename T, typename... Args>
+        T& AddComponent(EntityID entity, Args&&... args)
+        {
+            PECS_ASSERT(entity < m_MaxEntities && "AddComponent called with invalid Entity ID");
+
+            return GetOrCreatePool<T>().Add(entity, std::forward<Args>(args)...);
+        }
+
+        template <typename T>
+        T& GetComponent(EntityID entity)
+        {
+            PECS_ASSERT(entity < m_MaxEntities && "AddComponent called with invalid Entity ID");
+
+            SparseSet<T>* pool = TryGetPool<T>();
+
+            PECS_ASSERT(pool && "Component type does not exist");
+            PECS_ASSERT(pool->Has(entity) && "Entity does not have component");
+
+            return pool->Get(entity);
+        }
+
+        template <typename T>
+        bool RemoveComponent(EntityID entity)
+        {
+            PECS_ASSERT(entity < m_MaxEntities && "RemoveComponent called with invalid Entity ID");
+
+            if (auto* pool = TryGetPool<T>())
+            {
+                return pool->Remove(entity);
+            }
+            return false;
+        }
+
+        template <typename T>
+        bool HasComponent(EntityID entity) const 
+        {
+            PECS_ASSERT(entity < m_MaxEntities && "HasComponent called with invalid Entity ID");
+
+            if (auto* pool = TryGetPool<T>())
+            {
+                return pool->Has(entity);
+            }
+            return false;
+        }
+
+        template<typename T>
+        SparseSet<T>& Iterate()
+        {
+            return GetOrCreatePool<T>();
+        }
+
+
+    private:
+        unsigned int m_MaxEntities;
+        EntityID m_NextEntity = 0;
+        std::vector<EntityID> m_FreeEntities;
+
+        std::unordered_map<std::type_index, ISparseSetWrapper*> m_ComponentPools;
+
+        template <typename T>
+        SparseSet<T>& GetOrCreatePool()
+        {
+            std::type_index key(typeid(T));
+            auto it = m_ComponentPools.find(key);
+            if (it != m_ComponentPools.end())
+            {
+                return static_cast<SparseSetWrapper<T>*>(it->second)->set;
+            }
+
+            auto* wrapper = new SparseSetWrapper<T>(m_MaxEntities);
+            m_ComponentPools.emplace(key, wrapper);
+            return wrapper->set;
+        }
+
+        template <typename T>
+        SparseSet<T>* TryGetPool()
+        {
+            std::type_index key(typeid(T));
+            auto it = m_ComponentPools.find(key);
+            if (it != m_ComponentPools.end())
+            {
+                return &static_cast<SparseSetWrapper<T>*>(it->second)->set;
+            }
+
+            return nullptr;
+        }
+
+    };
+}
+
+#endif
---
+++ README.md
@@ -0,0 +1,93 @@
+# Planetoid Entity Component System
+
+A fast, lightweight C++17 Entity Component System made as a hobby project.
+
+## Requirements
+
+- C++ 17 or newer
+- An STL with typeindex, unordered_map and vector
+
+## Features
+
+- Sparse Set based Entity Component System
+- Fast Create / Delete Entity with Entity ID reusing
+- O(1) Time Complexity component querying, adding and removing
+- Contiguous Component storage for fast iteration 
+
+## Planned Features
+
+In no particular order:
+- Entity Versioning
+- Multi-Component querying
+- System Scheduling
+- More robust component iteration utilities
+- Debugging tools
+- Custom memory allocator/deallocator
+- Examples
+- C / STL-free implementation
+
+## Installation
+
+Simply drop PECS.hpp into your project and #include
+
+## Usage
+
+```cpp
+
+// PECS Entity ID Type defaults to size_t but can be changed by defining PECS_CUSTOM_TYPE before including PECS.hpp
+// This is optional
+#include <cstdint>
+#define PECS_CUSTOM_TYPE uint64_t
+
+#include <PECS.hpp>
+
+// Components can be any struct or class that is move-constructible and move-assignable
+struct MyComponent
+{
+    MyComponent(float v)
+        : Value(v) {}
+    float Value = 0.0f;
+};
+
+PECSInstance ECS(1000); // Create an ECS Instance, Max Entities = 1000
+EntityID Entity = ECS.CreateEntity(); // Returns an ID
+
+// AddComponent<> takes type as template, Entity ID to add to and the constructor parameters
+// Returns a reference to the component so can be retrieved as auto& or explicit typing
+ECS.AddComponent<MyComponent>(Entity, 12.5f); 
+
+// Or use GetComponent<>() as per below
+MyComponent& Component = ECS.GetComponent<MyComponent>(Entity);
+
+// HasComponent<>() returns whether the entity has the given component.
+bool EntityHasMyComponent = ECS.HasComponent<MyComponent>(Entity); // Returns true here
+
+// Sparse Sets allow iteration through the dense component storage. Each entry is a private struct containing EntityID and type, so you can do either of the following:
+for (auto& [entity, component] : ECS.Iterate<MyComponent>())
+{
+    ...
+}
+
+for (auto entry : ECS.Iterate<MyComponent>())
+{
+    EntityID entity = entry.entity;
+    MyComponent& component = entry.data;
+    ...
+}
+
+// RemoveComponent<> does exactly what you'd expect - removes the component.
+ECS.RemoveComponent<MyComponent>(Entity);
+
+// And DeleteEntity also does what it says on the tin. This will also remove all components from all sparse sets associated to this entity
+ECS.DeleteEntity(Entity);
+```
+
+This library implements a macro called PECS_ASSERT for assertions - feel free to define this before including to replace functionality
+
+## Contributing
+
+Not accepting external contributions yet, but this may change as time goes on - watch this space!
+
+## License
+
+[LICENSE](license) - MIT
